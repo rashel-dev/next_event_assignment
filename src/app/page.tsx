1bf9f4df48d61d5e69ea0dd25d281f55a35ff533
rashel9255 <ibrahim2000167@gmail.com>
2026-01-18 10:10:33 +0600

    feat: create hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import HeroSection from "@/components/pages/home/heroSection";
 
 export default function Home() {
   return (
     <div>
-      <h2>Hello World</h2>
-      <Button>Click me</Button>
+      <HeroSection />
     </div>
   );
 }
