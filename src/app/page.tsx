07c8cf2248b021070336fb618beb68e628bea244
rashel9255 <ibrahim2000167@gmail.com>
2026-01-18 10:17:31 +0600

    feat: create testimonial and subscriber section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 import HeroSection from "@/components/pages/home/heroSection";
 import { ServiceSection } from "@/components/pages/home/ServiceSection";
+import { SubscribeSection } from "@/components/pages/home/SubscribeSection";
+import { TestimonialSection } from "@/components/pages/home/TestimonialSection";
 
 export default function Home() {
   return (
     <div>
       <HeroSection />
       <ServiceSection />
+      <TestimonialSection />
+      <SubscribeSection />
     </div>
   );
 }
