92d2d2f2c35789ced494a1a6e2dec419d2ce6887
rashel9255 <ibrahim2000167@gmail.com>
2026-01-23 02:43:02 +0600

    fetch and show all events data

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -1,11 +1,74 @@
 "use client";
 
-const page = () => {
-    return (
-        <div>
-            <h2>This is events page</h2>
-        </div>
-    );
-};
+import { Button } from "@/components/ui/button";
+import { TEvent } from "@/types/event";
+import { Eye } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import { useEffect, useState } from "react";
 
-export default page;
+export default function EventsPage() {
+  const [events, setEvents] = useState<TEvent[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchEvents() {
+      try {
+        const res = await fetch("/api/events");
+        const data = await res.json();
+        setEvents(data.data);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchEvents();
+  }, []);
+
+
+  if (loading) return <p className="text-center mt-10">Loading events...</p>;
+  return (
+    <section className="container mx-auto px-4 py-8">
+      <h2 className="text-3xl font-semibold mb-12 text-center">All Events</h2>
+
+      <div className="grid gap-8 md:grid-cols-2 lg:grid-cols-3">
+        {events.map((event: TEvent) => (
+          <div
+            key={event._id}
+            className="relative overflow-hidden rounded-2xl bg-white shadow-lg transition-shadow duration-300 hover:shadow-2xl"
+          >
+            <div className="relative h-64 w-full">
+              <Image
+                src={event.image}
+                alt={event.title}
+                fill
+                className="object-cover"
+              />
+            </div>
+
+            <div className="p-5">
+              <h3 className="text-2xl font-semibold">{event.title}</h3>
+              <p className="mt-1 text-sm text-muted-foreground">
+                {event.date} | {event.location}
+              </p>
+              <p className="mt-3 line-clamp-3 text-gray-700">
+                {event.description}
+              </p>
+
+              <Link href={`/events/${event._id}`}>
+                <Button
+                  variant="outline"
+                  className="mt-4 flex items-center gap-2"
+                >
+                  <Eye size={18} /> View Details
+                </Button>
+              </Link>
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+}
--- src/types/event.ts
+++ src/types/event.ts
@@ -1,5 +1,6 @@
 export interface TEvent {
   id?: string;
+  _id?: string;
   title: string;
   date: string;
   location: string;
